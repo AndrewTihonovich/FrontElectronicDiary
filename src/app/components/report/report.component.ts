02c057dcc34d74319c11c419fcb1017b2b076d50
Andrew Tihonovich <AndrewT_87@tut.by>
2022-03-11 11:39:04 +0300

    develop_Add user info to report page

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { UserInfo } from "src/app/models/User/UserInfo";
+import { authenticationService } from "src/app/services/authentication.service";
 import { Record } from "../../models/Record/Record";
 import { reportService } from "../../services/report.service";
 
@@ -8,18 +10,27 @@ import { reportService } from "../../services/report.service";
     selector: 'report-page',
     templateUrl: 'report.component.html',
     styleUrls: ['report.component.css'],
-    providers : [reportService]
+    providers : [reportService, authenticationService]
 })
 export class ReportComponent {
     timeStart:Date;
     timeEnd:Date;
     userId:string;
+    sortUserId:string;
     records:Array<Record>;
 
-    constructor(private reportServ: reportService, public router: Router, private http: HttpClient){}
+    constructor(private authdServ: authenticationService, private reportServ: reportService, public router: Router, private http: HttpClient){}
    
+    ngOnInit(){
+        this.authdServ.UserInformation(this.getCurrentUser()).subscribe((data:UserInfo)=>this.userId=data.userLogin);
+    }
+
+    getCurrentUser():string{
+        return sessionStorage.getItem("currentUser");
+    }
+
     sort(Start:Date , End:Date){
-        this.reportServ.getRecordByTime(Start, End, this.userId ).subscribe((x:Array<Record>) => {this.records = x} );
+        this.reportServ.getRecordByTime(Start, End, this.sortUserId ).subscribe((x:Array<Record>) => {this.records = x} );
         
     }
 }
